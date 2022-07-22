67f49f595ebaac94a5d61e60109e95d9e579a245
Henry Andralojc <henry.andralojc@live.co.uk>
2022-07-22 17:42:55 +0100

    corrected error with central FD

--- firstDerivative.m
+++ firstDerivative.m
@@ -3,7 +3,6 @@ function dadz = firstDerivative(a, accuracy)
 % front and back difference derivatives to a given accuracy
 %%% JUST TO 2nd ORDER SO FAR, ignores whatever accuracy is given
 
-accuracy = 2;
 dadz = zeros(size(a));
 dz = 1/(length(a)-1);
 
@@ -16,13 +15,12 @@ if accuracy == 1
     dadz(2:end-1) = 0.5*(a(3:end) - a(1:end-2))/dz;
 
 elseif accuracy == 2
-    
     % Calculate derivative on the left boundary using forward finite
     % difference (second order)
     dadz(1) = (-1.5*a(1) + 2*a(2) -0.5*a(3))/dz;
     % Calculate derivative on the right boundary using backward finite
     % diff. (second order)
-    dadz(end) = (3.2*a(end) - 2*a(end-1) + 0.5*a(end-2))/dz;
+    dadz(end) = (1.5*a(end) - 2*a(end-1) + 0.5*a(end-2))/dz;
     % Calculate derivative in interior using central finite diff. (2nd
     % order)
     dadz(2:end-1) = 0.5*(a(3:end) - a(1:end-2))/dz;
