29d5e3a5150b4eace3f07102f802f93c72c7492e
Henry Andralojc <henry.andralojc@live.co.uk>
2022-07-26 13:28:19 +0100

    first 'working' draft

--- pSwellingDynamics.m
+++ pSwellingDynamics.m
@@ -4,62 +4,69 @@
 % Initialise parameters
 params.k = 1; % dimensionless permeability
 params.Omega = 720; % Ratio of strand to solvent volumes
-params.A = -62.22;
-params.B = 0.20470;
-params.phi0 = 0.05; % starting porosity temperature is 315K
-params.chi = @(T) params.A + params.B .* T; % chi parameter is a linear function of dimensionless temperature
+params.a = -62.22;
+params.b = 0.20470;
+params.phi0 = 0.1; % starting porosity
+params.chi = @(T) params.a + params.b .* T; % chi parameter is a linear function temperature
 
 %%%% Initialise porosity, temperature and position arrays
-Nzs = 10; % Number of points to sample phi and temperature at
-z = linspace(0, 1, Nzs); % z array of positions between 0 and 1
-dz = 1/(length(z)-1); % spatial step
+Nzs = 10^2; % Number of points to sample phi and temperature at
+z = linspace(0, 1, Nzs).'; % z array of positions between 0 and 1
+dz = 1/(Nzs-1); % spatial step
 
 tRange = [0 1]; % time range simulation goes over
-Nts = 1000000; % Number of time steps t = j/Nts
-dt = (tRange(end)-tRange(1))/Nts; % time step
+Nts = 4*10^7; % Number of time steps t = j/Nts
+dt = (tRange(end)-tRange(1))/(Nts-1); % time step
 
 phi = ones(Nzs,1)*params.phi0;
 % Starting eqm porosity is 0.05 - the temperature at this porosity:
 f = @(x, T) params.chi(T) + x.^2 *(x/params.Omega + 1./x + log(1-1./x));
 x = 1/(1-params.phi0);
 fun = @(T) f(x, T);
-params.T0 = fzero(fun, 310);
+params.T0 = fzero(fun, 310, optimset('Display', 'off'));
 
-% Temperature is then changed to T = 300 K
-temp = ones(Nzs, 1) * 300;
+% Temperature is then changed to T = 305 K
+T1 = 305;
+temp = ones(Nzs, 1) * T1;
 
 % Initial length of the gel is, by definition = 1 (in dimensionless units)
 h = 1;
-
-% the porosity of the gel at the free boundary will be constant, and is
-% determined by the phi that solves elasticStress = osmoticPressure
+nSteps = 10^6;
+hArray = zeros(nSteps, 1);
+hArray(1) = h;
 phi(end) = boundaryPhi(params, temp(end));
 
-% Calculate the effective stress and osmotic pressure in the bulk:
-sigmaP = elasticStress(phi, temp, params);
-Pi = osmoticPressure(phi, temp, params);
- 
-gradMu = firstDerivative(sigmaP, 2) - firstDerivative(Pi, 2);
+for step = 2:(nSteps+1)    
+    % Calculate the effective stress and osmotic pressure in the bulk:
+    sigmaP = elasticStress(phi, temp, params);
+    Pi = osmoticPressure(phi, temp, params);
 
-hRate = params.k / h * gradMu(end);
+    % Check stress balance at ends:
+    % if abs(sigmaP(end) - Pi(end)) > 0.1
+    %     string('HELP')
+    % end
+    
+    % Calculate gradient in chemical potential 
+    gradMu = firstDerivative(sigmaP, 2) - firstDerivative(Pi, 2);
+    % NO FLUX THROUGH LEFT BOUNDARY:
+    gradMu(1) = 0;
 
-h = h + hRate * dt;
+    % Calculate the rate at which the length is changing
+    hRate = params.k ./ h .* gradMu(end);
 
+    dfdt = hRate./h .* z .* firstDerivative(phi, 2) + 1/(h^2) .* firstDerivative((params.k .* (1-phi) .* gradMu),2);
+    
+    h = h + hRate .* dt;
+    phi = phi + dfdt .* dt;
 
+%     phi(phi>1) = 0.999;
+    hArray(step) = h;
+    % the porosity of the gel at the free boundary is determined by the phi 
+    % that solves elasticStress = osmoticPressure at temp of the boundary
+    phi(end) = boundaryPhi(params, temp(end));
+end
 
-% t = tRange(1);
-% % for step = 2:Nts+1
-% %     % Calculate the effective stress and osmotic pressure
-% %     sigmaP = elasticStress
-% %     
-% %     % time at the start of the loop is at time of previous step
-% %     hRate = lengthEvolution(h(step-1), temp(:,step-1), phi(:,step-1), params);
-% %     h = h + hRate*dt;
-% % %     temp = temp;
-% %     
-% %     phiRate = phiEvolution(h, phi, temp, params);
-% %     phi = phi + phiRate*dt;
-% %     
-% % 
-% %    t = t+dt;
-% % end
+hArray
+figure(1);
+plot((1:(nSteps+1)).*dt, hArray);
+ylim([0, 2])
