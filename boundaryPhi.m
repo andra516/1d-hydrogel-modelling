38b90eae8776e8ada31a4ffca679bae35fed2685
Henry Andralojc <henry.andralojc@live.co.uk>
2022-07-26 13:26:05 +0100

    stopped output of fsolve

--- boundaryPhi.m
+++ boundaryPhi.m
@@ -1,15 +1,15 @@
 function phiBoundary = boundaryPhi(params, tempBoundary)
-
 % Applies the BC at the free boundary sigmaP - Pi = 0, and finds the value of 
-% lambda, here denoted x, which satisfies it. This value is constant
-% throughout.
+% lambda, here denoted x, which satisfies it.
+% If tempBoundary is > 330, this fails
+
 T = tempBoundary;
 % BC is written in terms of lambda
-BCFunc = @(x) x + params.Omega .* (log(1-1/x) + 1/x + params.chi(T)/(x^2));
+BCFunc = @(x) x + params.Omega .* (log(1-1./x) + 1./x + params.chi(T)/(x.^2));
 
 % fsolve finds the x that satisfies BCFunc = 0
-xBoundary = fsolve(BCFunc, 1.1);
+xBoundary = fsolve(BCFunc, 1.1, optimset('Display', 'off'));
 
-phiBoundary = 1 - 1/xBoundary;
+phiBoundary = 1 - 1./xBoundary;
 
 end
