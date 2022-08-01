08b62078feefb5c3f001e97bf6e7d70f7509c939
Henry Andralojc <henry.andralojc@live.co.uk>
2022-08-01 15:35:50 +0100

    added fwd and bkwd finite diffs to 3rd order

--- firstDerivative.m
+++ firstDerivative.m
@@ -24,6 +24,17 @@ elseif accuracy == 2
     % Calculate derivative in interior using central finite diff. (2nd
     % order)
     dadz(2:end-1) = 0.5*(a(3:end) - a(1:end-2))./dz;
+    
+elseif accuracy == 3 % JUst for fwd and bkwd derivatives
+        % Calculate derivative on the left boundary using forward finite
+    % difference (second order)
+    dadz(1) = (-11/6*a(1) + 3*a(2) - 1.5*a(3) + 1/3*a(4))./dz;
+    % Calculate derivative on the right boundary using backward finite
+    % diff. (second order)
+    dadz(end) = (11/6*a(end) - 3*a(end-1) + 1.5*a(end-2) - 1/3*a(end-3))./dz;
+    % Calculate derivative in interior using central finite diff. (2nd
+    % order)
+    dadz(2:end-1) = 0.5*(a(3:end) - a(1:end-2))./dz;
 end 
 
 end
