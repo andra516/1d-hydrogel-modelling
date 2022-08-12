5e6d71b343f1e9e7a990bf0401267fab9c2c8eb0
Henry Andralojc <henry.andralojc@live.co.uk>
2022-08-12 17:11:01 +0100

    changed eqm condition function

--- boundaryPhi.m
+++ boundaryPhi.m
@@ -1,15 +1,17 @@
 function phiBoundary = boundaryPhi(params, tempBoundary)
-% Applies the BC at the free boundary sigmaP - Pi = 0, and finds the value of 
+% Applies the BC at the free boundary stress = 0, and finds the value of 
 % lambda, here denoted x, which satisfies it.
 % If tempBoundary is > 330, this fails
 
 T = tempBoundary;
 % BC is written in terms of lambda
-BCFunc = @(x) x + params.Omega .* (log(1-1./x) + 1./x + params.chi(T)/(x.^2));
+stress = @(x) T./params.T0 .* (x + params.Omega.*(log(1-1./x) ...
+                            + 1./x + params.chi(T)./(x.^2)));
+
 
 % fsolve finds the x that satisfies BCFunc = 0
-xBoundary = fsolve(BCFunc, 1.1, optimset('Display', 'off'));
+xBoundary = fsolve(stress, 1.1, optimset('Display', 'off'));
 
-phiBoundary = 1 - 1./xBoundary;
+phiBoundary = 1-1./xBoundary
 
 end
